469084e2a6bbdeca13080c316fcc2d4652976838
shivdutt-B <shivdutt059@gmail.com>
2025-06-18 22:38:54 +0530

    remove exact

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,10 +15,10 @@ function App() {
     <>
       <NavBar />
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/auth/*" element={<AuthPage />} />
-        <Route exact path="/docs" element={<DocsPage />} />
-        <Route exact path="/dashboard/*" element={<Dashboard />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth/*" element={<AuthPage />} />
+        <Route path="/docs" element={<DocsPage />} />
+        <Route path="/dashboard/*" element={<Dashboard />} />
       </Routes>
       <Footer />
     </>
