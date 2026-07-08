19ed39d8627d91cb0c0a466a7ec9095acc643336
shivdutt-B <shivdutt059@gmail.com>
2026-07-08 11:34:41 +0530

    add supported platforms logos

--- client/src/layouts/HomeLayout.tsx
+++ client/src/layouts/HomeLayout.tsx
@@ -1,4 +1,5 @@
 import RippleHero from "../components/Home/RippleHero";
+import LogoTicker from "../components/Home/LogoTicker";
 import StepsHero from "../components/Home/StepsHero";
 import { VideoHero } from "../components/Home/VideoHero";
 import FeatureHero from "../components/Home/FeatureHero";
@@ -8,6 +9,7 @@ function Home() {
   return (
     <>
       <RippleHero />
+      <LogoTicker />
       <StepsHero />
       <VideoHero />
       <FeatureHero />
