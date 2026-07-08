71e93c41c7cbde8a9d1c4ddbb1e50889adc1a779
shivdutt-B <shivdutt059@gmail.com>
2026-07-08 10:23:04 +0530

    basic redesiging done with grid patterns in the bg

--- client/src/components/Home/RippleHero.tsx
+++ client/src/components/Home/RippleHero.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Ripple } from "../Helper/RippleText";
 import HeroOnRipple from "./HeroOnRipple";
+
 const RippleHero: React.FC = () => {
   return (
-    <div className="relative h-[700px] w-full overflow-hidden">
-      <Ripple />
-      <HeroOnRipple />
+    <div className="relative w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 sm:py-24 flex flex-col items-center justify-center min-h-[500px] overflow-hidden">
+      {/* Fading Grid Background */}
+      <div className="absolute inset-0 fading-grid-bg pointer-events-none z-0" />
+      
+      <div className="w-full z-10">
+        <HeroOnRipple />
+      </div>
     </div>
   );
 };
